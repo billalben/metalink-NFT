fb73ffd6a0fb9bd5753f5c0ecf4da9837b613a00
Benzazoua <billalben252@gmail.com>
2023-12-27 18:53:03 +0100

    Collection Section
    - HTML structure
    - CSS style
    - logic of slider by JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,77 @@ const activeElementOnScroll = function () {
 };
 
 window.addEventListener("scroll", activeElementOnScroll);
+
+// Slider
+const sliders = document.querySelectorAll("[data-slider]");
+
+const sliderInit = function (slider) {
+  // Scoped within each slider
+  const sliderContainer = slider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = slider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = slider.querySelector("[data-slider-next]");
+
+  // Calculation of total visible items and total items
+  const totalSliderVisibleItems = Number(
+    getComputedStyle(slider).getPropertyValue("--slider-item")
+  );
+  const totalSliderItems =
+    sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  let currentSlidePos = 0;
+
+  // Update button visibility
+  function updateButtonVisibility() {
+    sliderPrevBtn.disabled = currentSlidePos === 0;
+    sliderNextBtn.disabled = currentSlidePos >= totalSliderItems;
+  }
+
+  // Move slider item
+  function moveSliderItem() {
+    const child = sliderContainer.children[currentSlidePos];
+    if (child) {
+      sliderContainer.style.transform = `translateX(-${child.offsetLeft}px)`;
+      updateButtonVisibility();
+    }
+  }
+
+  // Next slide function
+  function slideNext() {
+    currentSlidePos =
+      currentSlidePos >= totalSliderItems ? 0 : currentSlidePos + 1;
+    moveSliderItem();
+  }
+
+  // Previous slide function
+  function slidePrev() {
+    currentSlidePos =
+      currentSlidePos <= 0 ? totalSliderItems : currentSlidePos - 1;
+    moveSliderItem();
+  }
+
+  // Event listeners for next and previous buttons
+  sliderNextBtn.addEventListener("click", slideNext);
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  // Auto slide functionality
+  let autoSlideInterval;
+  function startAutoSlide() {
+    autoSlideInterval = setInterval(slideNext, 4000);
+  }
+  function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+  }
+
+  slider.addEventListener("mouseover", stopAutoSlide);
+  slider.addEventListener("mouseout", startAutoSlide);
+
+  // Responsive adjustment on window resize
+  window.addEventListener("resize", moveSliderItem);
+
+  // Initialize button visibility and auto sliding
+  updateButtonVisibility();
+  startAutoSlide();
+};
+
+// Initialize each slider
+sliders.forEach(sliderInit);
